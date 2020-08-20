eb7ac16c0088a2c7d80a1527fe9551c421e11d7c
Rafal <rafal_slowik@o2.pl>
2020-08-20 08:06:33 +0200

    test branch 2

---
+++ src/main/java/receipt/ReceiptGenerator.java
@@ -0,0 +1,22 @@
+package receipt;
+
+import item.Item;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Created by rafal on 10/02/2018.
+ */
+public interface ReceiptGenerator {
+
+    /**
+     *
+     * @param items
+     * @param totalToPay
+     * @param totalDiscount
+     * @return
+     * @throws Exception
+     */
+    String createReceipt(List<Item> items, BigDecimal totalToPay, BigDecimal totalDiscount) throws Exception;
+}

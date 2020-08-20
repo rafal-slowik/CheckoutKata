4251263bb8c106e78957315ed2291fa92c863cfc
Rafal Slowik <rafal2308@gmail.com>
2020-08-20 09:19:59 +0200

    Branch1 (#13)
    merge branch 1
    Co-authored-by: Rafal <rafal_slowik@o2.pl>

--- src/main/java/receipt/ReceiptGenerator.java
+++ src/main/java/receipt/ReceiptGenerator.java
@@ -10,13 +10,5 @@ import java.util.List;
  */
 public interface ReceiptGenerator {
 
-    /**
-     *
-     * @param items
-     * @param totalToPay
-     * @param totalDiscount
-     * @return
-     * @throws Exception
-     */
     String createReceipt(List<Item> items, BigDecimal totalToPay, BigDecimal totalDiscount) throws Exception;
 }
